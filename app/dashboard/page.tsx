7c38a01e28766869666a0c75ae2af2d05fa92f89
Sakib Muhtasim <sakif1600@gmail.com>
2026-07-09 10:04:15 +0600

    protected dashboard route

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const dashboard = () => {
-  return <div>dashboard page</div>;
+
+const dashboard = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers() // you need to pass the headers object.
+})
+  if(!session){
+    redirect('/signin')
+  }else{
+    return(
+      <div>
+        Welcome, {session.user.name}
+      </div>
+    )
+  }
 };
 
 export default dashboard;
